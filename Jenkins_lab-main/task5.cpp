d42cb8fb941e662f07d06fcf2e675036618041e1
Vishwa-Mehta <vishwamehta2002@gmail.com>
2023-02-16 20:54:58 +0530

    added Jenkinsfile task5

---
+++ Jenkins_lab-main/task5.cpp
@@ -0,0 +1,11 @@
+//File: task5.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cout << "Welcome to task 5. \n";
+	cout << "Hello PES2UG20CS389! \n";
+	cout << "Task 5 completed :)\n";
+	return 0;
+}
+
